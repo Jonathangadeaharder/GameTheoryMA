22c13b9a7df9688039ed822c0dd724ac472249e9
Jonathangadeaharder <44533402+Jonathangadeaharder@users.noreply.github.com>
2022-05-31 15:10:46 +0200

    deleted sever

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app'
-
-import '../styles/globals.css'
-import 'katex/dist/katex.css'
-
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
-
-export default MyApp
